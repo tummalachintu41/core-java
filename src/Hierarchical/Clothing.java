82d9d5fa4c38f69e2c1701ae182b379720020c01
tummalachintu41 <tummalachintu66@gmail.com>
2025-04-09 11:32:01 +0530

    Update Clothing.java

--- src/Hierarchical/Clothing.java
+++ src/Hierarchical/Clothing.java
@@ -1,4 +1,16 @@
 package Hierarchical;
 
-public class Clothing {
+  public class Clothing extends Product{
+    public String size;
+
+    public Clothing(String name, int price) {
+        super(name, price);
+        this.size=size;
+    }
+
+    public void ClothingDetails(){
+        ProductDetails();
+        System.out.println("size:"+size);
+    }
 }
+
