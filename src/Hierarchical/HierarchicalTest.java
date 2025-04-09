fc0d1d288cbe1425ad1ef3efd01c825b177393cf
tummalachintu41 <tummalachintu66@gmail.com>
2025-04-09 11:34:28 +0530

    Update HierarchicalTest.java

--- src/Hierarchical/HierarchicalTest.java
+++ src/Hierarchical/HierarchicalTest.java
@@ -1,4 +1,13 @@
 package Hierarchical;
-
 public class HierarchicalTest {
+    public static void main(String[] args) {
+        Book book=new Book("java",2500);
+        Mobile mobile=new Mobile("Iphone",100000);
+        Clothing clothing = new Clothing("shirt",1000);
+
+        book.BookDetails();
+        mobile.MobileDetails();
+        clothing.ClothingDetails();
+    }
 }
+
