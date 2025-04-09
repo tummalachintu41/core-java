c771166b519cb1034c6e0dd42bac5bfc96b60e7d
tummalachintu41 <tummalachintu66@gmail.com>
2025-04-09 11:35:06 +0530

    Update Mobile.java

--- src/Hierarchical/Mobile.java
+++ src/Hierarchical/Mobile.java
@@ -1,4 +1,14 @@
 package Hierarchical;
+public class Mobile extends Product{
+    public int Warranty;
 
-public class Mobile {
+    public Mobile(String name, int price) {
+        super(name, price);
+        this.Warranty=Warranty;
+    }
+    public void MobileDetails(){
+        ProductDetails();
+        System.out.println("Warranty:"+Warranty);
+    }
 }
+
